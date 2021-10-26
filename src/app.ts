b41975196bb828fd0fb93007d7c47535105fa85c
阮传宙 <chuanzhou1012@163.com>
2021-10-27 01:40:18 +0800

    feat: 如果是生产模式就是压缩中间件

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import middleware from './utils/composeMiddleware'
 import compress from 'koa-compress' // 压缩中间件
 import './mongodb/db'
 const app = new Koa()
+
+const isProdMode = process.env.NODE_ENV === 'production'
+
 // 计算中间价使用的时间
 app.use(async (ctx, next) => {
   const start = Date.now()
@@ -14,10 +17,12 @@ app.use(async (ctx, next) => {
   logUtil.info(`log output info`)
 })
 
-// 压缩中间件
-app.use(compress())
 app.use(middleware).use(router.routes()).use(router.allowedMethods())
 
+// 如果是开发环境压缩中间件
+if (isProdMode) {
+  app.use(compress())
+}
 app.on('error', (err, ctx) => {
   console.log(err)
   logUtil.error(err.stack)
